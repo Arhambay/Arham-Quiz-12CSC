d40a6a3471a668670e94c04986ae7393440a0f50
Arham Marzook <100536818+Arhambay@users.noreply.github.com>
2022-06-09 21:47:29 +0000

    First component (starting page)

--- main.py
+++ main.py
@@ -1,12 +1,111 @@
-import tkinter as tk
+from tkinter import *
+import random
+from PIL import Image, ImageTk
 
-window = tk.Tk()
-window.title("Hello wold")
-window.geometry("300x300")
+names_list = []
 
-hello = tk.Label(text="Hello world!")
-hello.pack()
-button = tk.Button(text="Click me!")
-button.pack()
 
-tk.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class QuizStarter:
+
+  def __init__(self, parent):
+ 
+  
+    #users input is taken by an Entry Widget
+    self.entry_box=Entry(parent)
+    self.entry_box.place(x=325, y=210)
+
+  #continue Button
+    self.continue_button = Button (parent, text="START QUIZ", bg="azure3", command=self.name_collection)
+    self.continue_button.place(x=355, y=250)
+
+  def name_collection(self):
+    name = self.entry_box.get()
+    names_list.append(name)
+    print(names_list)
+    #Quiz(root)
+
+
+
+
+#************** Starting Point of Program *************#
+if __name__ == '__main__':
+  root = Tk()
+  root.title('Basketball General Knowledge Quiz')
+  root.geometry('500x300')
+  bg_image = Image.open('real basketball.png')
+  bg_image = bg_image.resize((500, 300),Image.ANTIALIAS)
+  bg_image = ImageTk.PhotoImage(bg_image)
+  image_label= Label(root, image=bg_image)
+  image_label.place(x=0, y=0, relwidth=1, relheight=1)
+  quiz_starter_window = QuizStarter(root)
+  root.mainloop()
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
