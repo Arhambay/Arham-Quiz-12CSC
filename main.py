36367d6e724bae160aa137226fd837f26c409139
Arham Marzook <100536818+Arhambay@users.noreply.github.com>
2022-06-14 23:53:13 +0000

    questions continued/finished

--- main.py
+++ main.py
@@ -47,10 +47,47 @@ questions_answers = {
         '3 points' #item 6, index 5 will be the write statement we need to display the right statement if the users enters wrong choice
       ,4], #item 7,index 6 will be the position of the right answer (index whre right answer sits), this will be out check if answer is correct or not
 
-    6: ["",
-        
-      
-    ]
+    6: ["What is the glass called that the rim is attached to?", #item 1 index 0 will be the question
+        'Backboard', #item 2, index 1 will be first choice
+        'Frame', #item 3, index 2 will be second choice
+        'Mirror', #item 4, index 3 will be thrid choice
+        'Box',  #item 5, index 4 will be fourth choice
+        'Backboard'  #item 6, index 5 will be the write statement we need to display the right statement if the users enters wrong choice
+        ,1], #item 7,index 6 will be the position of the right answer (index whre right answer sits), this will be out check if answer is correct or not
+
+    7: ["How long are defending players allowed to stand in the paint for?", #item 1 index 0 will be the question
+        '10 seconds', #item 2, index 1 will be first choice
+        '24 seconds', #item 3, index 2 will be second choice
+        '5 seconds', #item 4, index 3 will be thrid choice
+        '3 seconds', #item 5, index 4 will be fourth choice
+        '3 seconds' #item 6, index 5 will be the write statement we need to display the right statement if the users enters wrong choice
+        ,4], #item 7,index 6 will be the position of the right answer (index whre right answer sits), this will be out check if answer is correct or not
+
+    8: ["When play is stopped and a player takes shots at the foul line, what are those shots called?", #item 1 index 0 will be the question
+        'jumpers', #item 2, index 1 will be first choice
+        'Free Throws', #item 3, index 2 will be second choice
+        'Dunk from a distance',  #item 4, index 3 will be thrid choice
+        'Free Shots', #item 5, index 4 will be fourth choice
+        'Free Throws' #item 6, index 5 will be the write statement we need to display the right statement if the users enters wrong choice
+        ,2], #item 7,index 6 will be the position of the right answer (index whre right answer sits), this will be out check if answer is correct or not
+
+    9: ["When your team gets possession, you have a limited number of time based on what?" #item 1 index 0 will be the question
+        'Shooting Timer', #item 2, index 1 will be first choice
+        'Possession Clock', #item 3, index 2 will be second choice
+        'Shot Clock',  #item 4, index 3 will be thrid choice
+        'Possession Timer', #item 5, index 4 will be fourth choice
+        'Shot Clock' #item 6, index 5 will be the write statement we need to display the right statement if the users enters wrong choice
+        ,3], #item 7,index 6 will be the position of the right answer (index whre right answer sits), this will be out check if answer is correct or not
+
+  
+    10: ["How many fouls does an NBA player get before they foul out?", #item 1 index 0 will be the question
+         '6', #item 2, index 1 will be first choice
+         '5', #item 3, index 2 will be second choice
+         '3',  #item 4, index 3 will be thrid choice
+         'limitless', #item 5, index 4 will be fourth choice
+         '6' #item 6, index 5 will be the write statement we need to display the right statement if the users enters wrong choice
+        ,1], #item 7,index 6 will be the position of the right answer (index whre right answer sits), this will be out check if answer is correct or not
+  
   
   
 
